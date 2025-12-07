38b241048fb287128d791aca0f71ced9aa3ffac5
abnerLouren <abnerjaredejede@gmail.com>
2025-12-07 17:37:37 +0100

    1st onde

---
+++ src/test/java/ao/sudojed/ls2/Ls2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.sudojed.ls2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ls2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
